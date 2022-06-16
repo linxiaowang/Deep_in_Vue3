72c4f9480207275c119728d5161afd4952981d11
linxiaowang <1104220717@qq.com>
2022-06-16 21:41:57 +0800

    feat: add auto import plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    Vue({
+      reactivityTransform: true,
+    }),
+    Components(),
+    AutoImport({
+      imports: ['vue',]
+    }),
+  ]
 })
